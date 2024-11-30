e4c2665b89df68205eea220d51930e8487460177
felivalencia <nbyfceryhxet@hotmail.com>
2024-11-29 22:12:06 -0300

    took out navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import Menu from './components/menu/Menu';
 import Login from './components/register/Login';
 import Signup from './components/register/Signup';
@@ -10,12 +9,8 @@ import Store from './components/store/Store';
 import './styles/styles.css';
 
 const AppContent = () => {
-  const location = useLocation();
-  const showNavbar = !['/game', '/store'].includes(location.pathname);
-
   return (
     <>
-      {showNavbar && <Navbar />}
       <Routes>
         <Route path="/" element={<Menu />} />
         <Route path="/store" element={<Store />} />
