a12756f989c83ecffb7cfeca7b1a3f77104d78d7
felivalencia <nbyfceryhxet@hotmail.com>
2024-11-30 14:57:46 -0300

    merge w mathews

--- server/server.js
+++ server/server.js
@@ -1,19 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
-//import supabase from './dbController.js';
+import supabase from './dbController.js';
 const app = express();
 dotenv.config();
 const port = 8080;
-
 app.get('/', async (req, res) => {
-  const { data, error } = await supabase.from('User').select('*');
+  
+  const { data, error } = await supabase.from('User').select("*");
   console.log('data :', data);
-
-  if (error) return res.status(500).json({ error: error.message });
-
+  if (error)
+    return res.status(500).json({ error: error.message });
   res.json(data);
 });
+app.get('/leaderBoard', async (req, res) => {
+  const { data, error } = await supabase.from('HighScore')
+  .select('name, User!inner(username)')
+  .eq('User.name', 'Indonesia');
+});
+app.post('login');
+app.post('/createUser');
+app.get('*') //is that how 404's are done?
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-});
+})
