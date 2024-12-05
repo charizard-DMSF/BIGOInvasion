5f5575ae17527e394c6f366c3be1320585ddb3cf
Daniel Koch <daniel95va@gmail.com>
2024-12-04 18:44:01 -0700

    danController

--- server/server.js
+++ server/server.js
@@ -1,9 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import supabase from './dbController.js';
+import supabase from './dbModel.js';
+import dbController from './dbController.js';
 const app = express();
 dotenv.config();
 const port = 8080;
+
+app.get('/leaders', dbController.getTopScores, async (req, res) => {
+  res.status(200).json({'data': res.locals})
+});
+
+app.post('/newScore', dbController.addHighScore, async (req, res) => {
+  console.log("addHighScore Activated")
+  res.sendStatus(200)
+});
+
+
 app.get('/', async (req, res) => {
   
   const { data, error } = await supabase.from('User').select("*");
@@ -12,18 +24,15 @@ app.get('/', async (req, res) => {
     return res.status(500).json({ error: error.message });
   res.json(data);
 });
-
-
-app.get('/leaderBoard', async (req, res) => {
-  const { data, error } = await supabase.from('HighScore')
-  .select('name, User!inner(username)')
-  .eq('User.name', 'Indonesia');
+app.get('/leaders', dbController.getTopScores, async (req, res) => {
+  console.log("in the server function")
+  res.status(500)
 });
 
 
 app.post('login');
 app.post('/createUser');
-app.get('*') //is that how 404's are done?
+app.get('*', () => res.sendStatus(404)) //is that how 404's are done?
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
